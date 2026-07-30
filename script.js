318fd8c897a658e5f486e481da02637efbf0e7a2
sampreetialive <sampreetighosh08@gmail.com>
2026-07-30 23:14:50 +0530

    Added sounds and confetti effects

--- script.js
+++ script.js
@@ -1,67 +1,77 @@
-const cells = document.querySelectorAll(".cell");
+const cells=document.querySelectorAll(".cell");
 
-const statusText = document.getElementById("status");
+const statusText=document.getElementById("status");
 
-const restartButton = document.getElementById("restart");
+const restartButton=document.getElementById("restart");
 
-const winningLine = document.querySelector(".winning-line");
-
-
-
-let currentPlayer = "X";
-
-let gameActive = true;
+const winningLine=document.querySelector(".winning-line");
 
 
+const clickSound=document.getElementById("clickSound");
 
-let scores = {
+const winSound=document.getElementById("winSound");
 
-    X:0,
+const drawSound=document.getElementById("drawSound");
 
-    O:0,
 
-    draw:0
+
+let currentPlayer="X";
+
+let gameActive=true;
+
+
+
+let gameState=[
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+];
+
+
+
+let scores={
+
+X:0,
+
+O:0,
+
+draw:0
 
 };
 
 
 
-const xScore = document.getElementById("x-score");
+const xScore=document.getElementById("x-score");
 
-const oScore = document.getElementById("o-score");
+const oScore=document.getElementById("o-score");
 
-const drawScore = document.getElementById("draw-score");
-
-
-
-let gameState = [
-
-    "","","",
-
-    "","","",
-
-    "","",""
-
-];
+const drawScore=document.getElementById("draw-score");
 
 
 
 
 
-const winningConditions = [
+const winningConditions=[
 
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
+[0,1,2],
+[3,4,5],
+[6,7,8],
 
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
 
-    [0,4,8],
-    [2,4,6]
+[0,4,8],
+[2,4,6]
 
 ];
+
 
 
 
@@ -70,31 +80,28 @@ const winningConditions = [
 function handleCellClick(){
 
 
-    let clickedIndex =
-    this.getAttribute("data-index");
+let index=this.dataset.index;
+
+
+if(gameState[index]!="" || !gameActive)
+return;
 
 
 
-    if(gameState[clickedIndex] !== "" || !gameActive){
-
-        return;
-
-    }
+gameState[index]=currentPlayer;
 
 
-
-    gameState[clickedIndex] = currentPlayer;
-
-
-    this.textContent = currentPlayer;
+this.textContent=currentPlayer;
 
 
+clickSound.play();
 
-    checkWinner();
 
+checkWinner();
 
 
 }
+
 
 
 
@@ -103,104 +110,110 @@ function handleCellClick(){
 function checkWinner(){
 
 
-    let winningCondition = null;
+let win=null;
 
 
 
-    for(let condition of winningConditions){
+for(let condition of winningConditions){
 
 
+let[a,b,c]=condition;
 
-        let a = gameState[condition[0]];
 
-        let b = gameState[condition[1]];
+if(
+gameState[a] &&
+gameState[a]==gameState[b] &&
+gameState[a]==gameState[c]
+){
 
-        let c = gameState[condition[2]];
+win=condition;
 
+break;
 
+}
 
-        if(a !== "" && a===b && b===c){
+}
 
 
-            winningCondition = condition;
 
-            break;
 
 
-        }
+if(win){
 
 
-    }
+statusText.textContent=
+`Player ${currentPlayer} Wins 🎉`;
 
 
+scores[currentPlayer]++;
 
 
+updateScore();
 
-    if(winningCondition){
 
+showLine(win);
 
 
-        statusText.textContent =
-        `Player ${currentPlayer} wins!`;
+winSound.play();
 
 
 
-        scores[currentPlayer]++;
+confetti({
 
+particles:200,
 
-        updateScore();
+spread:120
 
+});
 
 
-        showWinningLine(winningCondition);
 
+gameActive=false;
 
 
-        gameActive=false;
+return;
 
 
-        return;
+}
 
 
-    }
 
 
 
+if(!gameState.includes("")){
 
 
-    if(!gameState.includes("")){
+statusText.textContent=
+"Draw!";
 
 
-        statusText.textContent =
-        "It's a Draw!";
+scores.draw++;
 
 
-        scores.draw++;
+updateScore();
 
 
-        updateScore();
+drawSound.play();
 
 
-        gameActive=false;
 
+gameActive=false;
 
-        return;
 
+return;
 
-    }
 
+}
 
 
 
 
+currentPlayer=
+currentPlayer==="X"?"O":"X";
 
-    currentPlayer =
-    currentPlayer==="X" ? "O" : "X";
 
-
-
-    statusText.textContent =
-    `Player ${currentPlayer}'s Turn`;
+statusText.textContent=
+`Player ${currentPlayer}'s Turn`;
 
 
 
@@ -210,59 +223,41 @@ function checkWinner(){
 
 
 
-function showWinningLine(condition){
 
 
-
-    let positions = {
-
-
-        "0,1,2":
-        "translateY(-100px)",
+function showLine(win){
 
 
-        "3,4,5":
-        "translateY(0px)",
+winningLine.style.width="300px";
 
 
-        "6,7,8":
-        "translateY(100px)",
+if(win[0]==0)
+winningLine.style.top="45px";
 
 
-        "0,3,6":
-        "rotate(90deg) translateY(100px)",
+if(win[0]==3)
+winningLine.style.top="156px";
 
 
-        "1,4,7":
-        "rotate(90deg) translateY(0px)",
+if(win[0]==6)
+winningLine.style.top="268px";
 
-
-        "2,5,8":
-        "rotate(90deg) translateY(-100px)",
-
-
-        "0,4,8":
-        "rotate(45deg)",
-
-
-        "2,4,6":
-        "rotate(-45deg)"
-
-    };
-
-
-
-    winningLine.style.width="300px";
-
-
-    winningLine.style.transform =
-    positions[condition.toString()];
 
 
 }
 
 
 
+
+function updateScore(){
+
+xScore.textContent=scores.X;
+
+oScore.textContent=scores.O;
+
+drawScore.textContent=scores.draw;
+
+}
 
 
 
@@ -270,86 +265,59 @@ function showWinningLine(condition){
 function restartGame(){
 
 
-
-    currentPlayer="X";
-
-
-    gameActive=true;
-
-
-
-    gameState=[
-
-        "","","",
-
-        "","","",
-
-        "","",""
-
-    ];
+gameState=[
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+];
 
 
-
-    statusText.textContent =
-    "Player X's Turn";
+currentPlayer="X";
 
 
+gameActive=true;
 
-    cells.forEach(cell=>{
 
-        cell.textContent="";
-
-    });
+statusText.textContent=
+"Player X's Turn";
 
 
 
-    winningLine.style.width="0";
+cells.forEach(cell=>{
+
+cell.textContent="";
+
+});
+
+
+
+winningLine.style.width="0";
 
 
 
 }
-
-
-
-
-
-
-function updateScore(){
-
-
-    xScore.textContent=scores.X;
-
-
-    oScore.textContent=scores.O;
-
-
-    drawScore.textContent=scores.draw;
-
-
-}
-
-
-
 
 
 
 
 cells.forEach(cell=>{
 
-
-    cell.addEventListener(
-        "click",
-        handleCellClick
-    );
-
+cell.addEventListener(
+"click",
+handleCellClick
+);
 
 });
 
 
 
-
-
 restartButton.addEventListener(
-    "click",
-    restartGame
+"click",
+restartGame
 );
