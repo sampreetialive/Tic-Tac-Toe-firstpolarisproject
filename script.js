4e800d4aea6d0c76ecb93f4120346f4c32528c03
sampreetialive <sampreetighosh08@gmail.com>
2026-07-31 00:34:07 +0530

    Refactor game logic and UI elements in script.js

--- script.js
+++ script.js
@@ -1,93 +1,91 @@
-*{
+const cells = document.querySelectorAll(".cell");
 
-box-sizing:border-box;
+const statusText = document.getElementById("status");
 
-font-family:"Segoe UI",Arial,sans-serif;
+const restartButton = document.getElementById("restart");
 
-}
+const winningLine = document.querySelector(".winning-line");
 
 
+const clickSound = document.getElementById("clickSound");
 
-body{
+const winSound = document.getElementById("winSound");
 
-margin:0;
+const drawSound = document.getElementById("drawSound");
 
-height:100vh;
 
-display:flex;
 
-justify-content:center;
+let currentPlayer = "X";
 
-align-items:center;
+let gameActive = true;
 
-overflow:hidden;
 
 
-background:
+let gameState = [
 
-radial-gradient(
-circle at top,
-#17002b,
-#030308 70%
-);
+"",
+"",
+"",
 
+"",
+"",
+"",
 
-color:white;
+"",
+"",
+""
 
-}
+];
 
 
 
+let scores = {
 
+X:0,
 
-.background-glow{
+O:0,
 
-position:absolute;
+draw:0
 
-width:600px;
+};
 
-height:600px;
 
 
-background:
 
-radial-gradient(
-circle,
-rgba(0,170,255,.3),
-transparent 70%
-);
 
+const xScore = document.getElementById("x-score");
 
+const oScore = document.getElementById("o-score");
 
-filter:blur(100px);
+const drawScore = document.getElementById("draw-score");
 
 
 
-animation:floatGlow 8s infinite alternate;
 
 
-}
 
 
 
-@keyframes floatGlow{
+const winningConditions = [
 
+[0,1,2],
 
-from{
+[3,4,5],
 
-transform:translate(-100px,-50px);
+[6,7,8],
 
-}
+[0,3,6],
 
+[1,4,7],
 
-to{
+[2,5,8],
 
-transform:translate(100px,50px);
+[0,4,8],
 
-}
+[2,4,6]
 
+];
 
-}
 
 
 
@@ -95,482 +93,28 @@ transform:translate(100px,50px);
 
 
 
-.container{
 
-position:relative;
+function playSound(sound){
 
-z-index:2;
 
-text-align:center;
+if(sound){
 
-padding:20px;
 
+sound.volume = 0.5;
 
-}
 
+sound.currentTime = 0;
 
 
 
+sound.play()
 
+.catch(error=>{
 
+console.log("Audio blocked:",error);
 
-/* TITLE */
+});
 
-
-h1{
-
-
-font-size:60px;
-
-
-letter-spacing:10px;
-
-
-color:white;
-
-
-
-text-shadow:
-
-
-0 0 15px white,
-
-0 0 40px white;
-
-
-
-margin-bottom:20px;
-
-
-}
-
-
-
-
-
-
-
-
-
-
-/* STATUS */
-
-
-#status{
-
-
-height:55px;
-
-
-font-size:26px;
-
-
-font-weight:bold;
-
-
-
-display:flex;
-
-
-justify-content:center;
-
-
-align-items:center;
-
-
-
-color:white;
-
-
-
-transition:.3s;
-
-
-}
-
-
-
-
-
-#status.win{
-
-
-font-size:38px;
-
-
-color:#39ff14;
-
-
-
-text-shadow:
-
-
-0 0 20px #39ff14,
-
-0 0 50px #39ff14;
-
-
-}
-
-
-
-
-
-
-#status.draw{
-
-
-font-size:38px;
-
-
-color:#ff1744;
-
-
-
-text-shadow:
-
-
-0 0 20px #ff1744,
-
-0 0 50px #ff1744;
-
-
-}
-
-
-
-
-
-
-
-
-
-/* SCOREBOARD */
-
-
-.scoreboard{
-
-
-display:flex;
-
-
-justify-content:center;
-
-
-gap:20px;
-
-
-margin:25px;
-
-
-}
-
-
-
-
-
-.score-card{
-
-
-width:120px;
-
-
-padding:15px;
-
-
-border-radius:20px;
-
-
-
-background:
-
-rgba(0,140,255,.15);
-
-
-
-border:
-
-2px solid #008cff;
-
-
-
-color:#00aaff;
-
-
-
-box-shadow:
-
-
-0 0 25px #008cff;
-
-
-
-transition:.3s;
-
-
-}
-
-
-
-
-.score-card:hover{
-
-
-transform:
-
-translateY(-8px);
-
-
-}
-
-
-
-
-
-.score-card h3{
-
-
-margin:0;
-
-
-font-size:20px;
-
-
-}
-
-
-
-.score-card p{
-
-
-font-size:35px;
-
-
-margin:10px 0 0;
-
-
-}
-
-
-
-
-
-
-
-
-
-/* BOARD */
-
-
-.board{
-
-
-position:relative;
-
-
-
-display:grid;
-
-
-
-grid-template-columns:
-
-repeat(3,110px);
-
-
-
-gap:15px;
-
-
-
-padding:15px;
-
-
-
-border-radius:25px;
-
-
-
-background:
-
-rgba(255,255,255,.05);
-
-
-
-box-shadow:
-
-
-0 0 40px rgba(0,170,255,.5);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.cell{
-
-
-height:110px;
-
-
-width:110px;
-
-
-
-display:flex;
-
-
-
-justify-content:center;
-
-
-
-align-items:center;
-
-
-
-font-size:65px;
-
-
-
-font-weight:900;
-
-
-
-cursor:pointer;
-
-
-
-border-radius:20px;
-
-
-
-background:
-
-rgba(255,255,255,.05);
-
-
-
-border:
-
-2px solid rgba(255,255,255,.2);
-
-
-
-transition:.3s;
-
-
-
-}
-
-
-
-
-
-.cell:hover{
-
-
-transform:scale(1.1);
-
-
-
-border-color:#00f5ff;
-
-
-
-box-shadow:
-
-0 0 30px #00f5ff;
-
-
-}
-
-
-
-
-
-
-
-
-.cell.x{
-
-
-color:#00f5ff;
-
-
-
-text-shadow:
-
-
-0 0 15px #00f5ff,
-
-0 0 40px #00f5ff;
-
-
-animation:pop .3s;
-
-
-}
-
-
-
-
-
-.cell.o{
-
-
-color:#ff0080;
-
-
-
-text-shadow:
-
-
-0 0 15px #ff0080,
-
-0 0 40px #ff0080;
-
-
-
-animation:pop .3s;
-
-
-}
-
-
-
-
-
-
-
-
-@keyframes pop{
-
-
-0%{
-
-transform:scale(0);
-
-}
-
-
-70%{
-
-transform:scale(1.3);
-
-}
-
-
-100%{
-
-transform:scale(1);
 
 }
 
@@ -585,130 +129,111 @@ transform:scale(1);
 
 
 
-/* WINNING LINE */
-
-
-.winning-line{
-
-
-position:absolute;
+function handleCellClick(){
 
 
 
-height:8px;
+let index=this.dataset.index;
 
 
 
-width:0;
+if(
 
+gameState[index]!=="" ||
 
+!gameActive
 
-background:#00f5ff;
+){
 
-
-
-border-radius:20px;
-
-
-
-z-index:50;
-
-
-
-box-shadow:
-
-
-0 0 20px #00f5ff,
-
-0 0 60px #00f5ff;
-
-
-
-transform-origin:left center;
-
-
-
-transition:
-
-width .5s ease,
-
-transform .5s ease;
-
-
-
-pointer-events:none;
-
-
-
-/* IMPORTANT FIX */
-
-grid-column:1 / -1;
-
-grid-row:1 / -1;
-
+return;
 
 }
 
 
 
 
+gameState[index]=currentPlayer;
+
+
+
+this.textContent=currentPlayer;
 
 
 
 
 
-button{
+this.classList.add(
 
+currentPlayer==="X"
 
-margin-top:35px;
+?
 
+"x"
 
-padding:15px 45px;
+:
 
-
-
-font-size:20px;
-
-
-
-border:none;
-
-
-
-border-radius:50px;
-
-
-
-cursor:pointer;
-
-
-
-color:white;
-
-
-
-background:
-
-linear-gradient(
-
-45deg,
-
-#008cff,
-
-#00f5ff
+"o"
 
 );
 
 
 
-box-shadow:
 
 
-0 0 30px #00f5ff;
+playSound(clickSound);
 
 
 
-transition:.3s;
+checkWinner();
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkWinner(){
+
+
+let win=null;
+
+
+
+for(let condition of winningConditions){
+
+
+let [a,b,c]=condition;
+
+
+
+if(
+
+gameState[a] &&
+
+gameState[a]===gameState[b] &&
+
+gameState[a]===gameState[c]
+
+){
+
+
+win=condition;
+
+
+break;
+
+
+}
 
 
 }
@@ -717,17 +242,62 @@ transition:.3s;
 
 
 
-button:hover{
 
 
-transform:scale(1.1);
+if(win){
 
 
 
-box-shadow:
+statusText.textContent=
+
+`Player ${currentPlayer} Wins 🎉`;
 
 
-0 0 60px #00f5ff;
+
+statusText.className="win";
+
+
+
+scores[currentPlayer]++;
+
+
+
+updateScore();
+
+
+
+showLine(win);
+
+
+
+playSound(winSound);
+
+
+
+
+if(window.confetti){
+
+
+confetti({
+
+particleCount:200,
+
+spread:100,
+
+origin:{y:.6}
+
+});
+
+
+}
+
+
+
+gameActive=false;
+
+
+
+return;
 
 
 }
@@ -739,47 +309,368 @@ box-shadow:
 
 
 
-@media(max-width:600px){
+
+if(!gameState.includes("")){
 
 
 
-h1{
+statusText.textContent=
 
-font-size:40px;
-
-}
+"It's a Draw!";
 
 
 
-
-.board{
-
-
-grid-template-columns:
-
-repeat(3,80px);
-
-
-}
+statusText.className="draw";
 
 
 
-
-.cell{
-
-
-height:80px;
-
-
-width:80px;
+scores.draw++;
 
 
 
-font-size:45px;
+updateScore();
+
+
+
+playSound(drawSound);
+
+
+
+gameActive=false;
+
+
+
+return;
 
 
 }
 
 
 
+
+
+
+
+currentPlayer =
+
+currentPlayer==="X"
+
+?
+
+"O"
+
+:
+
+"X";
+
+
+
+
+
+statusText.className="";
+
+
+
+statusText.textContent=
+
+`Player ${currentPlayer}'s Turn`;
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function showLine(win){
+
+
+
+const board=document.querySelector(".board");
+
+
+
+const start=cells[win[0]];
+
+const end=cells[win[2]];
+
+
+
+
+
+const boardRect=
+
+board.getBoundingClientRect();
+
+
+
+const startRect=
+
+start.getBoundingClientRect();
+
+
+
+const endRect=
+
+end.getBoundingClientRect();
+
+
+
+
+
+
+
+const x1=
+
+startRect.left+
+
+startRect.width/2-
+
+boardRect.left;
+
+
+
+const y1=
+
+startRect.top+
+
+startRect.height/2-
+
+boardRect.top;
+
+
+
+
+
+const x2=
+
+endRect.left+
+
+endRect.width/2-
+
+boardRect.left;
+
+
+
+const y2=
+
+endRect.top+
+
+endRect.height/2-
+
+boardRect.top;
+
+
+
+
+
+
+const distance=
+
+Math.sqrt(
+
+(x2-x1)**2+
+
+(y2-y1)**2
+
+);
+
+
+
+
+
+const angle=
+
+Math.atan2(
+
+y2-y1,
+
+x2-x1
+
+)
+
+*
+
+180/Math.PI;
+
+
+
+
+
+
+
+winningLine.style.width=
+
+`${distance}px`;
+
+
+
+winningLine.style.left=
+
+`${x1}px`;
+
+
+
+winningLine.style.top=
+
+`${y1}px`;
+
+
+
+winningLine.style.transform=
+
+`rotate(${angle}deg)`;
+
+
+}
+
+
+
+
+
+
+
+
+
+function updateScore(){
+
+
+xScore.textContent=scores.X;
+
+
+oScore.textContent=scores.O;
+
+
+drawScore.textContent=scores.draw;
+
+
+}
+
+
+
+
+
+
+
+
+
+function restartGame(){
+
+
+
+currentPlayer="X";
+
+
+gameActive=true;
+
+
+
+gameState=[
+
+"",
+"",
+"",
+
+"",
+"",
+"",
+
+"",
+"",
+""
+
+];
+
+
+
+
+
+cells.forEach(cell=>{
+
+
+cell.textContent="";
+
+
+cell.classList.remove(
+
+"x",
+
+"o"
+
+);
+
+
+});
+
+
+
+
+
+
+statusText.className="";
+
+
+
+statusText.textContent=
+
+"Player X's Turn";
+
+
+
+
+
+
+winningLine.style.width="0px";
+
+winningLine.style.transform=
+
+"rotate(0deg)";
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+cells.forEach(cell=>{
+
+
+cell.addEventListener(
+
+"click",
+
+handleCellClick
+
+);
+
+
+});
+
+
+
+
+
+
+
+restartButton.addEventListener(
+
+"click",
+
+restartGame
+
+);
