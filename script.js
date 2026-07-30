7b7d9bdb6092719b05993a85d0c88def2393a8c0
sampreetialive <sampreetighosh08@gmail.com>
2026-07-30 23:04:41 +0530

    Added working score counter

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ const restartButton = document.getElementById("restart");
 let currentPlayer = "X";
 let gameActive = true;
 
-let gameState = ["", "", "", "", "", "", "", "", ""];
+let scores = {
+    X: 0,
+    O: 0,
+    draw: 0
+};
+
+const xScore = document.getElementById("x-score");
+const oScore = document.getElementById("o-score");
+const drawScore = document.getElementById("draw-score");
+
+
+let gameState = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+];
+
 
 const winningConditions = [
+
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -18,12 +35,15 @@ const winningConditions = [
 
     [0, 4, 8],
     [2, 4, 6]
+
 ];
+
 
 
 function handleCellClick() {
 
     let clickedCell = this;
+
     let clickedIndex = clickedCell.getAttribute("data-index");
 
 
@@ -33,6 +53,7 @@ function handleCellClick() {
 
 
     gameState[clickedIndex] = currentPlayer;
+
     clickedCell.textContent = currentPlayer;
 
 
@@ -41,12 +62,15 @@ function handleCellClick() {
 }
 
 
+
+
 function checkWinner() {
 
     let roundWon = false;
 
 
     for (let condition of winningConditions) {
+
 
         let a = gameState[condition[0]];
         let b = gameState[condition[1]];
@@ -59,61 +83,142 @@ function checkWinner() {
 
 
         if (a === b && b === c) {
+
             roundWon = true;
+
             break;
+
         }
+
     }
 
+
+
+    // WINNER
 
     if (roundWon) {
 
-        statusText.textContent = 
+
+        statusText.textContent =
         `Player ${currentPlayer} wins!`;
 
+
+        scores[currentPlayer]++;
+
+
+        updateScore();
+
+
         gameActive = false;
+
+
         return;
 
     }
 
+
+
+
+    // DRAW
 
     if (!gameState.includes("")) {
 
-        statusText.textContent = "It's a Draw!";
+
+        statusText.textContent =
+        "It's a Draw!";
+
+
+        scores.draw++;
+
+
+        updateScore();
+
+
         gameActive = false;
+
+
         return;
 
     }
 
 
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+
+
+    // CHANGE PLAYER
+
+    currentPlayer =
+    currentPlayer === "X" ? "O" : "X";
+
 
     statusText.textContent =
     `Player ${currentPlayer}'s Turn`;
+
 }
+
 
 
 
 function restartGame() {
 
+
     currentPlayer = "X";
+
     gameActive = true;
 
-    gameState = ["", "", "", "", "", "", "", "", ""];
 
-    statusText.textContent = "Player X's Turn";
+    gameState = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
+
+
+    statusText.textContent =
+    "Player X's Turn";
+
 
 
     cells.forEach(cell => {
+
         cell.textContent = "";
+
     });
+
 
 }
 
 
 
+
+function updateScore() {
+
+
+    xScore.textContent = scores.X;
+
+    oScore.textContent = scores.O;
+
+    drawScore.textContent = scores.draw;
+
+
+}
+
+
+
+
+
 cells.forEach(cell => {
-    cell.addEventListener("click", handleCellClick);
+
+    cell.addEventListener(
+        "click",
+        handleCellClick
+    );
+
 });
 
 
-restartButton.addEventListener("click", restartGame);
+
+restartButton.addEventListener(
+    "click",
+    restartGame
+);
